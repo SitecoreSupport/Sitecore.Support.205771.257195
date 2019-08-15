0ab80f3a32bc8ecbe6b22eea29eec6716c23248a
RenatDoroshenko <renat.doroshenko@sitecore.com>
2019-08-15 09:04:42 +0300

    commit patch

---
+++ src/Sitecore.Support.205771.257195/Data/Fields/ReferenceField.cs
@@ -0,0 +1,38 @@
+﻿namespace Sitecore.Support.Data.Fields
+{
+    using Sitecore.Data;
+    using Sitecore.Data.Fields;
+    using Sitecore.Data.Items;
+    using Sitecore.Diagnostics;
+    using Sitecore.Links;
+
+    public class ReferenceField : Sitecore.Data.Fields.ReferenceField
+    {
+        public ReferenceField(Field innerField) : base(innerField)
+        {
+            Assert.ArgumentNotNull(innerField, "innerField");
+        }
+
+        public override void Relink(ItemLink itemLink, Item newLink)
+        {
+            Assert.ArgumentNotNull(itemLink, "itemLink");
+            Assert.ArgumentNotNull(newLink, "newLink");
+
+            // added check whether field point to deleted item. If no => skip relink
+            if (this.Path == itemLink.TargetItemID.ToString())
+            {
+                this.Path = newLink.ID.ToString();
+            }
+        }
+
+        public override void RemoveLink(ItemLink itemLink)
+        {
+            Assert.ArgumentNotNull(itemLink, "itemLink");
+            ID tempTargetID = (base.TargetID == ID.Null && ID.IsID(base.Path)) ? new ID(base.Path) : base.TargetID;
+            if (itemLink.TargetItemID == tempTargetID)
+            {
+                base.Clear();
+            }
+        }
+    }
+}
---
+++ src/Sitecore.Support.205771.257195/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.205771.257195")]
+[assembly: AssemblyProduct("Sitecore.Support.205771.257195")]
+[assembly: ComVisible(false)]
